4757806493422e7b120719eecebdcbcfa9978e0d
jfzhao88 <hzhao1203@gmail.com>
2016-10-31 16:50:30 -0400

    Android Service
    andro service tutorial

---
+++ HW5/AndroidServices/app/src/main/java/com/example/fai/androidservices/MyService.java
@@ -0,0 +1,30 @@
+package com.example.fai.androidservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+/**
+ * Created by Fai on 10/27/2016.
+ */
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+
+}
